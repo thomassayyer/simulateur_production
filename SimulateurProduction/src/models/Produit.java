c31efbd0d51c8700f18f7902e87fa4dc97aef8c6
Mialy <Mialy@idea-PC>
2016-12-04 13:13:06 +0100

    ajout class Produit

--- SimulateurProduction/src/models/Produit.java
+++ SimulateurProduction/src/models/Produit.java
@@ -1,5 +1,16 @@
 package models;
 
 public class Produit {
+	
+	private int id;
+	private String type;
+	private double duree;
+	
+	public Produit(int id, String type, double duree)
+	{
+		this.id = id;
+		this.type = type;
+		this.duree = duree;
+	}
 
 }
