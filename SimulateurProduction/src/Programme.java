2f0a1279a6ce5a68175f7592b2895b7c6a51a8ec
thomassayyer <thomas.sayer@outlook.fr>
2016-12-05 14:10:47 +0100

    Ajout du programme principal.

---
+++ SimulateurProduction/src/Programme.java
@@ -0,0 +1,16 @@
+/**
+ * Programme principal.
+ * Contient une méthode statique "main" s'exécutant en première lors de l'exécution
+ * de l'application.
+ * 
+ * @author Thomas SAYER
+ * @version 1.0
+ */
+public class Programme {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
