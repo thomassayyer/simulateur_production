6577da2bb099b356b4b38bb18a282323cddacc83
j.chevallier <j.chevallier@315-14.formation-cci12.fr>
2016-12-05 15:27:46 +0100

    Modification gamme.java (commentaire)

--- SimulateurProduction/src/models/Gamme.java
+++ SimulateurProduction/src/models/Gamme.java
@@ -5,18 +5,22 @@ import models.Operation;
 
 public class Gamme 
 {
-	
+	private Gamme premier;
 	private Operation valeur;
 	private Gamme suivant;
 	
+	/**
+	 * Constructeur d'une liste.
+	 */
 	public Gamme(Operation valeur, Gamme suivant) 
 	{
+		this.premier = ;
 		this.valeur = valeur;
 		this.suivant = suivant;
 	}
 	
 	/**
-	*Crée un élément de liste svide
+	*Crée un élément de liste vide
 	*@param valeur Opération
 	*/
 	public Gamme(Operation valeur) 
@@ -30,7 +34,7 @@ public class Gamme
 	 */
 	public void AfficheGamme()
 	{
-		
+		int taile = getLongeur() 
 	}
 	
 	// LA SUITE DES FONCTIONS SERVENT POUR LA LISTE CHAINEE
@@ -45,26 +49,41 @@ public class Gamme
 		return valeur;
 	}
 	
+	
+	/**
+	 * Modifie la valeur de la p
+	 */
 	public void setValeur(Operation valeur) 
 	{
 		this.valeur = valeur;
 	}
-	
+
+	/**
+	 * Retourne l'élement suivant dans la liste
+	 */
 	public Gamme getSuivant() 
 	{
 		return suivant;
 	}
-	
+
+	/**
+	 * Modifie l'élement suivant de la liste 
+	 */
 	public void setSuivant(Gamme suivant) 
 	{
 		this.suivant = suivant;
-		}
+	}
 	
-	/*
+	/**
+	 * Retourne le premier element de la liste
+	 */
 	public Gamme getPremier() {
 		return premier;
 		}
 	
+	/**
+	 * Retourne la longueur de la liste
+	 */
 	public int getLongueur() 
 	{
 		int longueur= 0;
@@ -75,5 +94,6 @@ public class Gamme
 		}
 		return longueur;
 		}
-		*/
+
 }
+
