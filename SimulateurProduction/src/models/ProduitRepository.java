6554b12bc338689ca04fcddf50ce92ee7e838a0d
thomassayyer <thomas.sayer@outlook.fr>
2016-12-09 14:24:51 +0100

    Création de la classe ProduitRepository

--- SimulateurProduction/src/models/ProduitRepository.java
+++ SimulateurProduction/src/models/ProduitRepository.java
@@ -1,3 +1,63 @@
-//pas compris ce que c'est
+package models;
 
-//TODO : Faire une liste de produits + Ajout produit (?)
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Stock une liste de produits disponibles.
+ * 
+ * @author Mialy ANDRIAMIARANTSOANAVALONA
+ * @version 1.0
+ */
+public class ProduitRepository
+{
+	/**
+	 * Liste de produits.
+	 */
+	private List<Produit> produits;
+	
+	/**
+	 * Constructeur non paramétré qui instancie la liste chainée vide.
+	 */
+	public ProduitRepository()
+	{
+		this.produits = new LinkedList<Produit>();
+	}
+	
+	/**
+	 * Constructeur paramétré avec liste de produits.
+	 * 
+	 * @param produits Produits à ajouter dans la liste chainée.
+	 */
+	public ProduitRepository(List<Produit> produits)
+	{
+		this();
+		
+		for (Produit p : produits)
+		{
+			this.produits.add(p);
+		}
+	}
+	
+	/**
+	 * Ajoute un produit dans la liste chainée.
+	 * 
+	 * @param p Produit à ajouter.
+	 * @return Vrai si le produit a été ajouté; faux sinon.
+	 */
+	public boolean ajouterProduit(Produit p)
+	{
+		return this.produits.add(p);
+	}
+	
+	/**
+	 * Retire un produit de la liste chainée.
+	 * 
+	 * @param p Produit à retirer.
+	 * @return Vrai si le produit a été retiré; faux sinon.
+	 */
+	public boolean retirerProduit(Produit p)
+	{
+		return this.produits.remove(p);
+	}
+}
