a6acade4cb013690ac0d0abb57606e642c232d05
Mialy <Mialy@idea-PC>
2016-12-10 17:09:10 +0100

    Ajout creerTypeMachine

--- SimulateurProduction/src/repositories/TypeMachineRepository.java
+++ SimulateurProduction/src/repositories/TypeMachineRepository.java
@@ -2,6 +2,8 @@ package repositories;
 
 import java.util.LinkedList;
 import java.util.List;
+
+import models.Machine;
 import models.TypeMachine;
 
 /**
@@ -24,5 +26,49 @@ public class TypeMachineRepository {
 	{
 		this.types = new LinkedList<TypeMachine>();
 	}
+	
+	/**
+	 * Ajoute un type de machine
+	 * 
+	 * @param typeMachine TypeMachine à ajouter
+	 * @return Vrai si le type de machine a bien été créé; faux sinon.
+	 */
+	public boolean ajouterTypeMachine(TypeMachine typeMachine)
+	{		
+		return this.types.add(typeMachine);
+	}
+
+private static boolean creerTypeMachine()
+{
+		System.out.print("Numéro : ");
+		int num = sc.nextInt();
+		
+		System.out.println();
+		
+		System.out.print("Libellé : ");
+		int libelle = sc.nextInt();
+		
+		System.out.println();
+		
+		TypeMachine type = TypeMachine.values()[sc.nextInt() - 1];
+		
+		try
+		{
+			//TODO
+		}
+		catch (Exception e)
+		{
+			System.out.println(e.getMessage());
+			return true;
+		}
+		
+		System.out.println("Type de machine créé");
+	
+	System.out.println("Nouveau type de machine ?");
+	System.out.println("1 - Oui");
+	System.out.println("0 - Non");
+
+	return sc.nextInt() == 1 ? true : false;
+}
 	
 }
