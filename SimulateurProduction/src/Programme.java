2c12a64fb5d229b307ba4591abff38b4fc41acb3
thomassayyer <thomas.sayer@outlook.fr>
2016-12-10 17:02:15 +0100

    Modification de la création de machines.

--- SimulateurProduction/src/Programme.java
+++ SimulateurProduction/src/Programme.java
@@ -40,27 +40,32 @@ public class Programme
 	 */
 	private static boolean creerMachine()
 	{
-			System.out.print("Numéro : ");
-			int num = sc.nextInt();
+		System.out.print("Numéro : ");
+		int num = sc.nextInt();
 			
-			System.out.println();
+		System.out.println();
 			
-			System.out.print("Stock max : ");
-			int stockMax = sc.nextInt();
+		System.out.print("Stock max : ");
+		int stockMax = sc.nextInt();
 			
-			System.out.println();
+		System.out.println();
 			
-			System.out.println("Type : ");
+		System.out.println("Type : ");
 			
-			int i = 1;
-			for (TypeMachine t : typesMachine.getTypes())
-			{
-				System.out.println(i + " - " + t);
-				i++;
-			}
+		int i = 1;
+		for (TypeMachine t : typesMachine.getTypes())
+		{
+			System.out.println(i + " - " + t);
+			i++;
+		}
 			
-			TypeMachine type = typesMachine.recuperer(sc.nextInt() - 1);
+		TypeMachine type = typesMachine.recuperer(sc.nextInt() - 1);
 			
+		System.out.println("Combien de machines voulez-vous créer ?");
+		int nbMachines = sc.nextInt();
+			
+		for (int j = 0; j < nbMachines; j++)
+		{
 			try
 			{
 				atelier.ajouterMachine(new Machine(num, stockMax, type));
@@ -70,8 +75,11 @@ public class Programme
 				System.out.println(e.getMessage());
 				return true;
 			}
+				
+			num++;
+		}
 			
-			System.out.println("Machine créée");
+		System.out.println("Machine(s) créée(s)");
 		
 		System.out.println("Nouvelle machine ?");
 		System.out.println("1 - Oui");
@@ -87,6 +95,8 @@ public class Programme
 	 */
 	public static void main(String[] args)
 	{
+		// TODO Création des types de machines
+		
 		System.out.println("Créer machine ?");
 		System.out.println("1 - Oui");
 		System.out.println("0 - Non");
@@ -102,5 +112,13 @@ public class Programme
 				nouvelleMachine = creerMachine();
 			} while (nouvelleMachine);
 		}
+		
+		// TODO Création des opérations.
+		
+		// TODO Création des gammes.
+		
+		// TODO Création des produits.
+		
+		// TODO Lancement de la simulation.
 	}
 }
